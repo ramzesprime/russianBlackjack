e9ddca5d8072a0ba97ded6737b3e26b6d107cf9c
миша <rulko.mkes@gmail.com>
2025-04-28 20:18:36 +0300

    add header

--- main/main.cpp
+++ main/main.cpp
@@ -1,13 +1,5 @@
-#include <iostream>
-#include <thread>
-#include <vector>
-#include <random>
-#include <windows.h>
-#include <mutex>
-#include <condition_variable>
-#include <cstdlib>
-#include <fstream>
-#include <vector>
+
+#include "PORNO.h"
 
 
 using namespace std;
---
+++ main/porno.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <random>
+#include <windows.h>
+#include <mutex>
+#include <condition_variable>
+#include <cstdlib>
+#include <fstream>
+#include <vector>
+
+
+#define PORNO
